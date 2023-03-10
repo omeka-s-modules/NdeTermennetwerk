7aa86b430d84bca0a811d82539bfc4bba4e72761
Jim Safley <jimsafley@gmail.com>
2023-03-10 17:30:05 -0500

    Disable module if ValueSuggest is not active

--- Module.php
+++ Module.php
@@ -2,11 +2,16 @@
 namespace NdeTermennetwerk;
 
 use Omeka\Module\AbstractModule;
+use ValueSuggest\Module as ValueSuggestModule;
 
 class Module extends AbstractModule
 {
     public function getConfig()
     {
+        // Disable this module if the ValueSuggest module is not active.
+        if (!class_exists(ValueSuggestModule::class, false)) {
+            return [];
+        }
         return include sprintf('%s/config/module.config.php', __DIR__);
     }
 }
